NICMx/FORT-validator

commit 330240b2b5de670858a492703f016da21cc374bd

Panic on ENOMEMs
Trying to recover is incorrect because we don't want to advertise an
incomplete or outdated VRP table to the routers. We don't want to rely
on the OOM-killer; we NEED to die on memory allocation failures ASAP.

Though this doesn't have much to do with the RRDP refactor, I'm doing it
early to get plenty of time for testing and review.

Partially F1xes #40. (Still need to update some dependency usages.)

src/visited_uris.c
@@ -22,25 +22,24 @@ struct visited_uris {
 DEFINE_ARRAY_LIST_STRUCT(uris_roots, char *);
 DEFINE_ARRAY_LIST_FUNCTIONS(uris_roots, char *, static)
 
-static int
-visited_elem_create(struct visited_elem **elem, char const *uri)
+static struct visited_elem *
+visited_elem_create(char const *uri)
 {
 	struct visited_elem *tmp;
 
 	tmp = malloc(sizeof(struct visited_elem));
 	if (tmp == NULL)
-		return pr_enomem();
+		enomem_panic();
 	/* Needed by uthash */
 	memset(tmp, 0, sizeof(struct visited_elem));
 
 	tmp->uri = strdup(uri);
 	if (tmp->uri == NULL) {
 		free(tmp);
-		return pr_enomem();
+		enomem_panic();
 	}
 
-	*elem = tmp;
-	return 0;
+	return tmp;
 }
 
 static void
@@ -50,20 +49,19 @@ visited_elem_destroy(struct visited_elem *elem)
 	free(elem);
 }
 
-int
-visited_uris_create(struct visited_uris **uris)
+struct visited_uris *
+visited_uris_create(void)
 {
 	struct visited_uris *tmp;
 
 	tmp = malloc(sizeof(struct visited_uris));
 	if (tmp == NULL)
-		return pr_enomem();
+		enomem_panic();
 
 	tmp->table = NULL;
 	tmp->refs = 1;
 
-	*uris = tmp;
-	return 0;
+	return tmp;
 }
 
 static void
@@ -100,22 +98,11 @@ elem_find(struct visited_uris *list, char const *uri)
 	return found;
 }
 
-int
+void
 visited_uris_add(struct visited_uris *uris, char const *uri)
 {
-	struct visited_elem *elem;
-	int error;
-
-	if (elem_find(uris, uri) != NULL)
-		return 0; /* Exists, don't add again */
-
-	elem = NULL;
-	error = visited_elem_create(&elem, uri);
-	if (error)
-		return error;
-
-	HASH_ADD_STR(uris->table, uri, elem);
-	return 0;
+	if (elem_find(uris, uri) == NULL)
+		HASH_ADD_STR(uris->table, uri, visited_elem_create(uri));
 }
 
 int
@@ -133,7 +120,7 @@ visited_uris_remove(struct visited_uris *uris, char const *uri)
 	return 0;
 }
 
-static int
+static void
 visited_uris_to_arr(struct visited_uris *uris, struct uris_roots *roots)
 {
 	struct visited_elem *elem;
@@ -145,13 +132,11 @@ visited_uris_to_arr(struct visited_uris *uris, struct uris_roots *roots)
 		size = last_slash - elem->uri;
 		tmp = malloc(size + 1);
 		if (tmp == NULL)
-			return pr_enomem();
+			enomem_panic();
 		strncpy(tmp, elem->uri, size);
 		tmp[size] = '\0';
 		uris_roots_add(roots, &tmp);
 	}
-
-	return 0;
 }
 
 static void
@@ -170,21 +155,17 @@ visited_uris_delete_local(struct visited_uris *uris, char const *workspace)
 	int error;
 
 	uris_roots_init(&roots);
-
-	error = visited_uris_to_arr(uris, &roots);
-	if (error)
-		goto err;
-
+	visited_uris_to_arr(uris, &roots);
 	if (roots.len == 0)
 		goto success;
 
 	error = delete_dir_daemon_start(roots.array, roots.len, workspace);
-	if (error)
-		goto err;
+	if (error) {
+		uris_roots_cleanup(&roots, uris_root_destroy);
+		return error;
+	}
+
 success:
 	uris_roots_cleanup(&roots, uris_root_destroy);
 	return 0;
-err:
-	uris_roots_cleanup(&roots, uris_root_destroy);
-	return error;
 }

commit d5a51f30bc484558b15b909eaaf2716f29f27a78

Fix multiple evaluation of HASH_ADD_STR argument
This bug was introduced by 330240b2b5de670858a492703f016da21cc374bd.

src/visited_uris.c
@@ -91,8 +91,13 @@ elem_find(struct visited_uris *list, char const *uri)
 void
 visited_uris_add(struct visited_uris *uris, char const *uri)
 {
-	if (elem_find(uris, uri) == NULL)
-		HASH_ADD_STR(uris->table, uri, visited_elem_create(uri));
+	struct visited_elem *node;
+
+	if (elem_find(uris, uri) == NULL) {
+		/* Do not inline; HASH_ADD_STR expands "add" multiple times. */
+		node = visited_elem_create(uri);
+		HASH_ADD_STR(uris->table, uri, node);
+	}
 }
 
 int