NICMx/FORT-validator

commit 604f845ce6b9eb596dc9f1fdff7a7fa5752fcc87

Improve the cache fallback algorithm
If all of a RPP's URLs fail, fall back to most sensible cached
candidate.
It seems this used to be only implemented for TAs, and the heuristics
for choosing a suitable fallback were rudimentary.
Elaborate, centralize and extend implementation to all cache content.

Side maintenance tweaks:

- Remove EREQFAILED, because it largely evolved from meaning "don't try
  again" to "try again." So now it was just a redundant EAGAIN.
- Ditch redundant arguments from valid_file_or_dir().
- Merge the three URI arraylist implementations (certificate.c, tal.c
  and manifest.c) into one.
- Move RRDP workspace URI, from a thread variable to the stack.
  (Code smell. It used to be awkward to follow this variable's lifespan
  through the tree traversal.)
- Move struct publish and struct withdraw from the heap to the stack.
  (Eliminate pointless allocations. These are not the only RRDP
  structures I want to move to the stack.)
- Change file_metadata.uri from `char *` to `struct rpki_uri *`.
  (This string was forcing the RRDP code to recompute the URI
  repeatedly.)

src/object/certificate.c
@@ -49,10 +49,8 @@ struct ski_arguments {
 	OCTET_STRING_t *sid;
 };
 
-STATIC_ARRAY_LIST(sia_rpp_uris, struct rpki_uri *)
-
 struct sia_uris {
-	struct sia_rpp_uris rpp;
+	struct uri_list rpp;
 	struct rpki_uri *mft;
 };
 
@@ -115,20 +113,14 @@ static const struct ad_metadata RPKI_MANIFEST = {
 static void
 sia_uris_init(struct sia_uris *uris)
 {
-	sia_rpp_uris_init(&uris->rpp);
+	uris_init(&uris->rpp);
 	uris->mft = NULL;
-}
-
-static void
-cleanup_uri(struct rpki_uri **uri)
-{
-	uri_refput(*uri);
 }
 
 static void
 sia_uris_cleanup(struct sia_uris *uris)
 {
-	sia_rpp_uris_cleanup(&uris->rpp, cleanup_uri);
+	uris_cleanup(&uris->rpp);
 	uri_refput(uris->mft);
 }
 
@@ -1214,7 +1206,7 @@ handle_caRepository(struct rpki_uri *uri, void *arg)
 {
 	struct sia_uris *uris = arg;
 	pr_val_debug("caRepository: %s", uri_val_get_printable(uri));
-	sia_rpp_uris_add(&uris->rpp, &uri);
+	uris_add(&uris->rpp, uri);
 	uri_refget(uri);
 	return 0;
 }
@@ -1224,7 +1216,7 @@ handle_rpkiNotify(struct rpki_uri *uri, void *arg)
 {
 	struct sia_uris *uris = arg;
 	pr_val_debug("rpkiNotify: %s", uri_val_get_printable(uri));
-	sia_rpp_uris_add(&uris->rpp, &uri);
+	uris_add(&uris->rpp, uri);
 	uri_refget(uri);
 	return 0;
 }
@@ -1531,7 +1523,7 @@ uri_create_ad(struct rpki_uri **uri, ACCESS_DESCRIPTION *ad, enum uri_type type)
 	 * directory our g2l version of @asn1_string should contain.
 	 * But ask the testers to keep an eye on it anyway.
 	 */
-	return __uri_create(uri, type,
+	return __uri_create(uri, type, NULL,
 	    ASN1_STRING_get0_data(asn1_string),
 	    ASN1_STRING_length(asn1_string));
 }
@@ -1927,63 +1919,15 @@ certificate_validate_aia(struct rpki_uri *caIssuers, X509 *cert)
 	return 0;
 }
 
-static bool
-try_uris(struct sia_uris *uris, enum uri_type const *filter)
-{
-	struct rpki_uri **node, *uri;
-	enum uri_type type;
-
-	ARRAYLIST_FOREACH(&uris->rpp, node) {
-		uri = *node;
-		type = uri_get_type(uri);
-
-		if (filter != NULL && (*filter) != type)
-			continue;
-
-		switch (type) {
-		case UT_RSYNC:
-			if (cache_download(uri, NULL) == 0)
-				return true;
-			break;
-		case UT_HTTPS:
-			if (rrdp_update(uri) == 0)
-				return true;
-			break;
-		default:
-			pr_crit("Unknown URI type: %u", type);
-		}
-	}
-
-	return false;
-}
-
-static int
+static struct rpki_uri *
 download_rpp(struct sia_uris *uris)
 {
-	static const enum uri_type HTTP = UT_HTTPS;
-	static const enum uri_type RSYNC = UT_RSYNC;
-
-	if (uris->rpp.len == 0)
-		return pr_val_err("SIA lacks both caRepository and rpkiNotify.");
-
-	if (config_get_http_priority() > config_get_rsync_priority()) {
-		if (try_uris(uris, &HTTP))
-			return 0;
-		if (try_uris(uris, &RSYNC))
-			return 0;
-
-	} else if (config_get_http_priority() < config_get_rsync_priority()) {
-		if (try_uris(uris, &RSYNC))
-			return 0;
-		if (try_uris(uris, &HTTP))
-			return 0;
-
-	} else {
-		if (try_uris(uris, NULL))
-			return 0;
+	if (uris->rpp.len == 0) {
+		pr_val_err("SIA lacks both caRepository and rpkiNotify.");
+		return NULL;
 	}
 
-	return pr_val_err("The RPP could not be downloaded.");
+	return uris_download(&uris->rpp, true);
 }
 
 /** Boilerplate code for CA certificate validation and recursive traversal. */
@@ -1995,6 +1939,7 @@ certificate_traverse(struct rpp *rpp_parent, struct rpki_uri *cert_uri)
 	STACK_OF(X509_CRL) *rpp_parent_crl;
 	X509 *cert;
 	struct sia_uris sia_uris;
+	struct rpki_uri *downloaded;
 	enum rpki_policy policy;
 	enum cert_type certype;
 	struct rpp *pp;
@@ -2061,17 +2006,21 @@ certificate_traverse(struct rpp *rpp_parent, struct rpki_uri *cert_uri)
 	if (error)
 		goto revert_uris;
 
-	error = download_rpp(&sia_uris);
-	if (error)
+	downloaded = download_rpp(&sia_uris);
+	if (downloaded == NULL) {
+		error = EINVAL;
 		goto revert_uris;
+	}
 
-	error = x509stack_push(validation_certstack(state), cert_uri,
-		    cert, policy, certype);
+	error = x509stack_push(validation_certstack(state), cert_uri, cert,
+	    policy, certype);
 	if (error)
 		goto revert_uris;
 	cert = NULL; /* Ownership stolen */
 
-	error = handle_manifest(sia_uris.mft, &pp);
+	error = handle_manifest(sia_uris.mft,
+	    (uri_get_type(downloaded) == UT_HTTPS) ? downloaded : NULL,
+	    &pp);
 	if (error) {
 		x509stack_cancel(validation_certstack(state));
 		goto revert_uris;
@@ -2082,7 +2031,6 @@ certificate_traverse(struct rpp *rpp_parent, struct rpki_uri *cert_uri)
 	rpp_refput(pp);
 
 revert_uris:
-	validation_set_notification_uri(state, NULL);
 	sia_uris_cleanup(&sia_uris);
 revert_cert:
 	if (cert != NULL)

src/object/tal.c
@@ -20,31 +20,17 @@
 
 typedef int (*foreach_uri_cb)(struct tal *, struct rpki_uri *, void *);
 
-struct uris {
-	struct rpki_uri **array; /* This is an array of rpki URIs. */
-	unsigned int count;
-	unsigned int size;
-};
-
 struct tal {
 	char const *file_name;
-	struct uris uris;
+	struct uri_list uris;
 	unsigned char *spki; /* Decoded; not base64. */
 	size_t spki_len;
 };
 
 struct validation_thread {
 	pthread_t pid;
-
 	/* TAL file name */
 	char *tal_file;
-	/*
-	 * Try to use the TA from the local cache? Only if none of the URIs
-	 * was sync'd.
-	 */
-	bool retry_local;
-	/* Try to sync the current TA URI? */
-	bool sync_files;
 	struct db_table *db;
 	int exit_status;
 	/* This should also only be manipulated by the parent thread. */
@@ -59,50 +45,27 @@ struct tal_param {
 	struct threads_list threads;
 };
 
-static void
-uris_init(struct uris *uris)
-{
-	uris->count = 0;
-	uris->size = 4; /* Most TALs only define one. */
-	uris->array = pmalloc(uris->size * sizeof(struct rpki_uri *));
-}
-
-static void
-uris_destroy(struct uris *uris)
-{
-	unsigned int i;
-	for (i = 0; i < uris->count; i++)
-		uri_refput(uris->array[i]);
-	free(uris->array);
-}
-
 static int
-uris_add(struct uris *uris, char *uri)
+add_uri(struct uri_list *uris, char *uri)
 {
 	struct rpki_uri *new;
 	int error;
 
 	if (str_starts_with(uri, "rsync://"))
-		error = uri_create(&new, UT_RSYNC, uri);
+		error = uri_create(&new, UT_RSYNC, NULL, uri);
 	else if (str_starts_with(uri, "https://"))
-		error = uri_create(&new, UT_HTTPS, uri);
+		error = uri_create(&new, UT_HTTPS, NULL, uri);
 	else
 		error = pr_op_err("TAL has non-RSYNC/HTTPS URI: %s", uri);
 	if (error)
 		return error;
 
-	if (uris->count + 1 >= uris->size) {
-		uris->size *= 2;
-		uris->array = realloc(uris->array,
-		    uris->size * sizeof(struct rpki_uri *));
-	}
-
-	uris->array[uris->count++] = new;
+	uris_add(uris, new);
 	return 0;
 }
 
 static int
-read_uris(struct line_file *lfile, struct uris *uris)
+read_uris(struct line_file *lfile, struct uri_list *uris)
 {
 	char *uri;
 	int error;
@@ -136,7 +99,7 @@ read_uris(struct line_file *lfile, struct uris *uris)
 	}
 
 	do {
-		error = uris_add(uris, uri);
+		error = add_uri(uris, uri);
 		free(uri); /* Won't be needed anymore */
 		if (error)
 			return error;
@@ -335,7 +298,7 @@ tal_load(char const *file_name, struct tal **result)
 	return 0;
 
 fail:
-	uris_destroy(&tal->uris);
+	uris_cleanup(&tal->uris);
 	free(tal);
 	lfile_close(lfile);
 	return error;
@@ -347,54 +310,9 @@ tal_destroy(struct tal *tal)
 	if (tal == NULL)
 		return;
 
-	uris_destroy(&tal->uris);
+	uris_cleanup(&tal->uris);
 	free(tal->spki);
 	free(tal);
-}
-
-static int
-foreach(enum uri_type const *filter, struct tal *tal,
-    foreach_uri_cb cb, void *arg)
-{
-	struct rpki_uri *uri;
-	unsigned int i;
-	int error;
-
-	for (i = 0; i < tal->uris.count; i++) {
-		uri = tal->uris.array[i];
-		if (filter == NULL || (*filter) == uri_get_type(uri)) {
-			error = cb(tal, uri, arg);
-			if (error)
-				return error;
-		}
-	}
-
-	return 0;
-}
-
-static int
-foreach_uri(struct tal *tal, foreach_uri_cb cb, void *arg)
-{
-	static const enum uri_type HTTP = UT_HTTPS;
-	static const enum uri_type RSYNC = UT_RSYNC;
-	int error;
-
-	if (config_get_http_priority() > config_get_rsync_priority()) {
-		error = foreach(&HTTP, tal, cb, arg);
-		if (!error)
-			error = foreach(&RSYNC, tal, cb, arg);
-
-	} else if (config_get_http_priority() < config_get_rsync_priority()) {
-		error = foreach(&RSYNC, tal, cb, arg);
-		if (!error)
-			error = foreach(&HTTP, tal, cb, arg);
-
-	} else {
-		error = foreach(NULL, tal, cb, arg);
-
-	}
-
-	return error;
 }
 
 char const *
@@ -415,7 +333,7 @@ tal_get_spki(struct tal *tal, unsigned char const **buffer, size_t *len)
  * have been extracted from a TAL.
  */
 static int
-handle_tal_uri(struct tal *tal, struct rpki_uri *uri, void *arg)
+handle_tal_uri(struct tal *tal, struct rpki_uri *uri, struct db_table *db)
 {
 	/*
 	 * Because of the way the foreach iterates, this function must return
@@ -432,45 +350,21 @@ handle_tal_uri(struct tal *tal, struct rpki_uri *uri, void *arg)
 	 */
 
 	struct validation_handler validation_handler;
-	struct validation_thread *thread;
 	struct validation *state;
 	struct cert_stack *certstack;
 	struct deferred_cert deferred;
 	int error;
 
-	thread = arg;
+	pr_val_debug("TAL URI '%s' {", uri_val_get_printable(uri));
 
 	validation_handler.handle_roa_v4 = handle_roa_v4;
 	validation_handler.handle_roa_v6 = handle_roa_v6;
 	validation_handler.handle_router_key = handle_router_key;
-	validation_handler.arg = thread->db;
+	validation_handler.arg = db;
 
 	error = validation_prepare(&state, tal, &validation_handler);
 	if (error)
 		return ENSURE_NEGATIVE(error);
-
-	if (thread->sync_files) {
-		error = cache_download(uri, NULL);
-		/* Reminder: there's a positive error: EREQFAILED */
-		if (error) {
-			validation_destroy(state);
-			return pr_val_warn(
-			    "TAL URI '%s' could not be downloaded.",
-			    uri_val_get_printable(uri));
-		}
-	} else {
-		/* Look for local files */
-		if (!valid_file_or_dir(uri_get_local(uri), true, false,
-		    pr_val_err)) {
-			validation_destroy(state);
-			return 0; /* Error already logged */
-		}
-	}
-
-	/* At least one URI was sync'd */
-	thread->retry_local = false;
-
-	pr_val_debug("TAL URI '%s' {", uri_val_get_printable(uri));
 
 	if (!uri_is_certificate(uri)) {
 		error = pr_op_err("TAL URI does not point to a certificate. (Expected .cer, got '%s')",
@@ -535,6 +429,7 @@ do_file_validation(void *arg)
 {
 	struct validation_thread *thread = arg;
 	struct tal *tal;
+	struct rpki_uri *ta_uri;
 	int error;
 
 	fnstack_init();
@@ -544,29 +439,14 @@ do_file_validation(void *arg)
 	if (error)
 		goto end;
 
-	error = foreach_uri(tal, handle_tal_uri, thread);
-	if (error > 0) {
-		error = 0;
-		goto destroy_tal;
-	} else if (error < 0) {
-		goto destroy_tal;
-	}
-
-	if (!thread->retry_local) {
+	ta_uri = uris_download(&tal->uris, false);
+	if (ta_uri == NULL) {
 		error = pr_op_err("None of the URIs of the TAL '%s' yielded a successful traversal.",
 		    thread->tal_file);
 		goto destroy_tal;
 	}
 
-	thread->sync_files = false;
-	pr_val_warn("Looking for the TA certificate at the local files.");
-
-	error = foreach_uri(tal, handle_tal_uri, thread);
-	if (error > 0)
-		error = 0;
-	else if (error == 0)
-		error = pr_op_err("None of the URIs of the TAL '%s' yielded a successful traversal.",
-		    thread->tal_file);
+	error = handle_tal_uri(tal, ta_uri, thread->db);
 
 destroy_tal:
 	tal_destroy(tal);
@@ -594,8 +474,6 @@ spawn_tal_thread(char const *tal_file, void *arg)
 	thread = pmalloc(sizeof(struct validation_thread));
 
 	thread->tal_file = pstrdup(tal_file);
-	thread->retry_local = true;
-	thread->sync_files = true;
 	thread->db = param->db;
 	thread->exit_status = -EINTR;
 	SLIST_INSERT_HEAD(&param->threads, thread, next);

src/types/uri.c
@@ -1,5 +1,7 @@
 #include "types/uri.h"
 
+#include <errno.h>
+
 #include "alloc.h"
 #include "common.h"
 #include "config.h"
@@ -9,6 +11,8 @@
 #include "thread_var.h"
 #include "config/filename_format.h"
 #include "data_structure/path_builder.h"
+#include "cache/local_cache.h"
+#include "rrdp/rrdp_loader.h"
 
 /**
  * Design notes:
@@ -331,20 +335,15 @@ map_simple(struct rpki_uri *uri, char const *gprefix, int err)
  * "<local-repository>/rrdp/<notification-path>/a.b.c/d/e.cer".
  */
 static int
-map_caged(struct rpki_uri *uri)
+map_caged(struct rpki_uri *uri, struct rpki_uri *notif)
 {
 	struct path_builder pb;
-	struct rpki_uri *notification;
 	int error;
-
-	notification = validation_get_notification_uri(state_retrieve());
-	if (notification == NULL)
-		pr_crit("Programming error: Notification not recorded.");
 
 	error = pb_init_cache(&pb, "rrdp");
 	if (error)
 		return error;
-	error = append_guri(&pb, notification->global, "https://", ENOTHTTPS, true);
+	error = append_guri(&pb, notif->global, "https://", ENOTHTTPS, true);
 	if (error)
 		return error;
 	error = append_guri(&pb, uri->global, "rsync://", ENOTRSYNC, true);
@@ -356,7 +355,7 @@ map_caged(struct rpki_uri *uri)
 }
 
 static int
-autocomplete_local(struct rpki_uri *uri)
+autocomplete_local(struct rpki_uri *uri, struct rpki_uri *notif)
 {
 	switch (uri->type) {
 	case UT_RSYNC:
@@ -364,7 +363,7 @@ autocomplete_local(struct rpki_uri *uri)
 	case UT_HTTPS:
 		return map_simple(uri, "https://", ENOTHTTPS);
 	case UT_CAGED:
-		return map_caged(uri);
+		return map_caged(uri, notif);
 	}
 
 	pr_crit("Unknown URI type: %u", uri->type);
@@ -375,8 +374,8 @@ autocomplete_local(struct rpki_uri *uri)
  * need to be NULL terminated, but I'm not sure.
  */
 int
-__uri_create(struct rpki_uri **result, enum uri_type type, void const *guri,
-    size_t guri_len)
+__uri_create(struct rpki_uri **result, enum uri_type type,
+    struct rpki_uri *notif, void const *guri, size_t guri_len)
 {
 	struct rpki_uri *uri;
 	int error;
@@ -391,7 +390,7 @@ __uri_create(struct rpki_uri **result, enum uri_type type, void const *guri,
 
 	uri->type = type;
 
-	error = autocomplete_local(uri);
+	error = autocomplete_local(uri, notif);
 	if (error) {
 		free(uri->global);
 		free(uri);
@@ -405,9 +404,10 @@ __uri_create(struct rpki_uri **result, enum uri_type type, void const *guri,
 }
 
 int
-uri_create(struct rpki_uri **result, enum uri_type type, char const *guri)
+uri_create(struct rpki_uri **result, enum uri_type type, struct rpki_uri *notif,
+    char const *guri)
 {
-	return __uri_create(result, type, guri, strlen(guri));
+	return __uri_create(result, type, notif, guri, strlen(guri));
 }
 
 /*
@@ -415,7 +415,8 @@ uri_create(struct rpki_uri **result, enum uri_type type, char const *guri)
  * names. This function will infer the rest of the URL.
  */
 int
-uri_create_mft(struct rpki_uri **result, struct rpki_uri *mft, IA5String_t *ia5)
+uri_create_mft(struct rpki_uri **result, struct rpki_uri *notif,
+    struct rpki_uri *mft, IA5String_t *ia5)
 {
 	struct rpki_uri *uri;
 	int error;
@@ -428,11 +429,9 @@ uri_create_mft(struct rpki_uri **result, struct rpki_uri *mft, IA5String_t *ia5)
 		return error;
 	}
 
-	uri->type = (validation_get_notification_uri(state_retrieve()) == NULL)
-	    ? UT_RSYNC
-	    : UT_CAGED;
+	uri->type = (notif == NULL) ? UT_RSYNC : UT_CAGED;
 
-	error = autocomplete_local(uri);
+	error = autocomplete_local(uri, notif);
 	if (error) {
 		free(uri->global);
 		free(uri);
@@ -455,6 +454,9 @@ uri_refget(struct rpki_uri *uri)
 void
 uri_refput(struct rpki_uri *uri)
 {
+	if (uri == NULL)
+		return;
+
 	uri->references--;
 	if (uri->references == 0) {
 		free(uri->global);
@@ -565,4 +567,96 @@ uri_op_get_printable(struct rpki_uri *uri)
 
 	format = config_get_op_log_filename_format();
 	return uri_get_printable(uri, format);
+}
+
+DEFINE_ARRAY_LIST_FUNCTIONS(uri_list, struct rpki_uri *, static)
+
+void
+uris_init(struct uri_list *uris)
+{
+	uri_list_init(uris);
+}
+
+static void
+__uri_refput(struct rpki_uri **uri)
+{
+	uri_refput(*uri);
+}
+
+void
+uris_cleanup(struct uri_list *uris)
+{
+	uri_list_cleanup(uris, __uri_refput);
+}
+
+/* Swallows @uri. */
+void
+uris_add(struct uri_list *uris, struct rpki_uri *uri)
+{
+	uri_list_add(uris, &uri);
+}
+
+static int
+download(struct rpki_uri *uri, bool use_rrdp)
+{
+	return (use_rrdp && (uri_get_type(uri) == UT_HTTPS))
+	    ? rrdp_update(uri)
+	    : cache_download(uri, NULL);
+}
+
+static struct rpki_uri *
+download_uris(struct uri_list *uris, enum uri_type type, bool use_rrdp)
+{
+	struct rpki_uri **cursor, *uri;
+	ARRAYLIST_FOREACH(uris, cursor) {
+		uri = *cursor;
+		if (uri_get_type(uri) == type && download(uri, use_rrdp) == 0)
+			return uri;
+	}
+	return NULL;
+}
+
+/**
+ * Assumes all the URIs are URLs, and represent different ways to access the
+ * same content.
+ *
+ * Sequentially (in the order dictated by their priorities) attempts to update
+ * (in the cache) the content pointed by each URL.
+ * Stops on the first success, returning the corresponding URI.
+ *
+ * If there's no successful update, attempts to find one that's already cached.
+ * Returns the newest successfully cached URI.
+ *
+ * Does not grab any references.
+ */
+struct rpki_uri *
+uris_download(struct uri_list *uris, bool use_rrdp)
+{
+	struct rpki_uri **cursor, *uri;
+
+	if (config_get_http_priority() > config_get_rsync_priority()) {
+		uri = download_uris(uris, UT_HTTPS, use_rrdp);
+		if (uri != NULL)
+			return uri;
+		uri = download_uris(uris, UT_RSYNC, use_rrdp);
+		if (uri != NULL)
+			return uri;
+
+	} else if (config_get_http_priority() < config_get_rsync_priority()) {
+		uri = download_uris(uris, UT_RSYNC, use_rrdp);
+		if (uri != NULL)
+			return uri;
+		uri = download_uris(uris, UT_HTTPS, use_rrdp);
+		if (uri != NULL)
+			return uri;
+
+	} else {
+		ARRAYLIST_FOREACH(uris, cursor) {
+			uri = *cursor;
+			if (download(uri, use_rrdp) == 0)
+				return uri;
+		}
+	}
+
+	return cache_recover(uris, use_rrdp);
 }

commit 0a50ef48d6fc64f1bc5a6b64b68ad6a18f7d04f5

Fix traversal result code
handle_tal_uri() was returning 0 on soft errors and positive on success.
It's supposed to be the other way around.

This resulted in the main loop dropping successful tree traversals.

It also resulted in TA public key mismatches causing traversal
termination, which was a violation of RFC 8630:

> If the connection to the preferred URI fails or the retrieved CA
> certificate public key does not match the TAL public key, the RP
> SHOULD retrieve the CA certificate from the next URI, according to
> the local preference ranking of URIs.

This bug was introduced by 604f845ce6b9eb596dc9f1fdff7a7fa5752fcc87.

src/object/certificate.c
@@ -1919,15 +1919,27 @@ certificate_validate_aia(struct rpki_uri *caIssuers, X509 *cert)
 	return 0;
 }
 
+static int
+retrieve_uri(struct rpki_uri *uri, void *arg)
+{
+	struct rpki_uri **result = arg;
+	*result = uri;
+	return 0;
+}
+
 static struct rpki_uri *
 download_rpp(struct sia_uris *uris)
 {
+	struct rpki_uri *uri;
+	int error;
+
 	if (uris->rpp.len == 0) {
 		pr_val_err("SIA lacks both caRepository and rpkiNotify.");
 		return NULL;
 	}
 
-	return uris_download(&uris->rpp, true);
+	error = uris_download(&uris->rpp, true, retrieve_uri, &uri);
+	return error ? NULL : uri;
 }
 
 /** Boilerplate code for CA certificate validation and recursive traversal. */

src/object/tal.c
@@ -40,9 +40,14 @@ struct validation_thread {
 /* List of threads, one per TAL file */
 SLIST_HEAD(threads_list, validation_thread);
 
-struct tal_param {
+struct tal_thread_args {
 	struct db_table *db;
 	struct threads_list threads;
+};
+
+struct handle_tal_args {
+	struct tal *tal;
+	struct db_table *db;
 };
 
 static int
@@ -330,25 +335,11 @@ tal_get_spki(struct tal *tal, unsigned char const **buffer, size_t *len)
 
 /**
  * Performs the whole validation walkthrough on uri @uri, which is assumed to
- * have been extracted from a TAL.
+ * have been extracted from TAL @tal.
  */
 static int
 handle_tal_uri(struct tal *tal, struct rpki_uri *uri, struct db_table *db)
 {
-	/*
-	 * Because of the way the foreach iterates, this function must return
-	 *
-	 * - 0 on soft errors.
-	 * - `> 0` on URI handled successfully.
-	 * - `< 0` on hard errors.
-	 *
-	 * A "soft error" is "the connection to the preferred URI fails, or the
-	 * retrieved CA certificate public key does not match the TAL public
-	 * key." (RFC 8630)
-	 *
-	 * A "hard error" is any other error.
-	 */
-
 	struct validation_handler validation_handler;
 	struct validation *state;
 	struct cert_stack *certstack;
@@ -367,9 +358,10 @@ handle_tal_uri(struct tal *tal, struct rpki_uri *uri, struct db_table *db)
 		return ENSURE_NEGATIVE(error);
 
 	if (!uri_is_certificate(uri)) {
-		error = pr_op_err("TAL URI does not point to a certificate. (Expected .cer, got '%s')",
+		pr_op_err("TAL URI does not point to a certificate. (Expected .cer, got '%s')",
 		    uri_op_get_printable(uri));
-		goto fail;
+		error = EINVAL;
+		goto end;
 	}
 
 	/* Handle root certificate. */
@@ -377,13 +369,13 @@ handle_tal_uri(struct tal *tal, struct rpki_uri *uri, struct db_table *db)
 	if (error) {
 		switch (validation_pubkey_state(state)) {
 		case PKS_INVALID:
-			error = 0; /* Try a different TAL URI. */
+			error = EINVAL;
 			goto end;
 		case PKS_VALID:
 		case PKS_UNTESTED:
-			goto fail; /* Reject the TAL. */
+			error = ENSURE_NEGATIVE(error);
+			goto end;
 		}
-
 		pr_crit("Unknown public key state: %u",
 		    validation_pubkey_state(state));
 	}
@@ -402,8 +394,7 @@ handle_tal_uri(struct tal *tal, struct rpki_uri *uri, struct db_table *db)
 	do {
 		error = deferstack_pop(certstack, &deferred);
 		if (error == -ENOENT) {
-			/* No more certificates left; we're done. */
-			error = 1;
+			error = 0; /* No more certificates left; we're done */
 			goto end;
 		} else if (error) /* All other errors are critical, currently */
 			pr_crit("deferstack_pop() returned illegal %d.", error);
@@ -418,10 +409,16 @@ handle_tal_uri(struct tal *tal, struct rpki_uri *uri, struct db_table *db)
 		rpp_refput(deferred.pp);
 	} while (true);
 
-fail:	error = ENSURE_NEGATIVE(error);
 end:	validation_destroy(state);
 	pr_val_debug("}");
 	return error;
+}
+
+static int
+__handle_tal_uri(struct rpki_uri *uri, void *arg)
+{
+	struct handle_tal_args *args = arg;
+	return handle_tal_uri(args->tal, uri, args->db);
 }
 
 static void *
@@ -429,7 +426,7 @@ do_file_validation(void *arg)
 {
 	struct validation_thread *thread = arg;
 	struct tal *tal;
-	struct rpki_uri *ta_uri;
+	struct handle_tal_args handle_args;
 	int error;
 
 	fnstack_init();
@@ -439,19 +436,15 @@ do_file_validation(void *arg)
 	if (error)
 		goto end;
 
-	ta_uri = uris_download(&tal->uris, false);
-	if (ta_uri == NULL) {
-		error = pr_op_err("None of the URIs of the TAL '%s' yielded a successful traversal.",
+	handle_args.tal = tal;
+	handle_args.db = thread->db;
+	error = uris_download(&tal->uris, false, __handle_tal_uri, &handle_args);
+	if (error)
+		pr_op_err("None of the URIs of the TAL '%s' yielded a successful traversal.",
 		    thread->tal_file);
-		goto destroy_tal;
-	}
 
-	error = handle_tal_uri(tal, ta_uri, thread->db);
-
-destroy_tal:
 	tal_destroy(tal);
-end:
-	fnstack_cleanup();
+end:	fnstack_cleanup();
 	thread->exit_status = error;
 	return NULL;
 }
@@ -467,16 +460,16 @@ thread_destroy(struct validation_thread *thread)
 static int
 spawn_tal_thread(char const *tal_file, void *arg)
 {
-	struct tal_param *param = arg;
+	struct tal_thread_args *thread_args = arg;
 	struct validation_thread *thread;
 	int error;
 
 	thread = pmalloc(sizeof(struct validation_thread));
 
 	thread->tal_file = pstrdup(tal_file);
-	thread->db = param->db;
+	thread->db = thread_args->db;
 	thread->exit_status = -EINTR;
-	SLIST_INSERT_HEAD(&param->threads, thread, next);
+	SLIST_INSERT_HEAD(&thread_args->threads, thread, next);
 
 	error = pthread_create(&thread->pid, NULL, do_file_validation, thread);
 	if (error) {
@@ -492,33 +485,33 @@ spawn_tal_thread(char const *tal_file, void *arg)
 int
 perform_standalone_validation(struct db_table *table)
 {
-	struct tal_param param;
+	struct tal_thread_args args;
 	struct validation_thread *thread;
 	int error, tmperr;
 
-	param.db = table;
-	SLIST_INIT(&param.threads);
+	args.db = table;
+	SLIST_INIT(&args.threads);
 
-	/* TODO (fine) Maybe don't use threads if there's only one TAL */
+	/* TODO (fine) Maybe don't spawn threads if there's only one TAL */
 	error = foreach_file(config_get_tal(), ".tal", true, spawn_tal_thread,
-	    &param);
+	    &args);
 	if (error) {
-		while (!SLIST_EMPTY(&param.threads)) {
-			thread = SLIST_FIRST(&param.threads);
-			SLIST_REMOVE_HEAD(&param.threads, next);
+		while (!SLIST_EMPTY(&args.threads)) {
+			thread = SLIST_FIRST(&args.threads);
+			SLIST_REMOVE_HEAD(&args.threads, next);
 			thread_destroy(thread);
 		}
 		return error;
 	}
 
 	/* Wait for all */
-	while (!SLIST_EMPTY(&param.threads)) {
-		thread = SLIST_FIRST(&param.threads);
+	while (!SLIST_EMPTY(&args.threads)) {
+		thread = SLIST_FIRST(&args.threads);
 		tmperr = pthread_join(thread->pid, NULL);
 		if (tmperr)
 			pr_crit("pthread_join() threw %d (%s) on the '%s' thread.",
 			    tmperr, strerror(tmperr), thread->tal_file);
-		SLIST_REMOVE_HEAD(&param.threads, next);
+		SLIST_REMOVE_HEAD(&args.threads, next);
 		if (thread->exit_status) {
 			error = thread->exit_status;
 			pr_op_warn("Validation from TAL '%s' yielded error %d (%s); discarding all validation results.",

src/types/uri.c
@@ -597,23 +597,35 @@ uris_add(struct uri_list *uris, struct rpki_uri *uri)
 }
 
 static int
-download(struct rpki_uri *uri, bool use_rrdp)
+download(struct rpki_uri *uri, bool use_rrdp, uris_dl_cb cb, void *arg)
 {
-	return (use_rrdp && (uri_get_type(uri) == UT_HTTPS))
+	int error;
+
+	error = (use_rrdp && (uri_get_type(uri) == UT_HTTPS))
 	    ? rrdp_update(uri)
 	    : cache_download(uri, NULL);
+	if (error)
+		return 1;
+
+	return cb(uri, arg);
 }
 
-static struct rpki_uri *
-download_uris(struct uri_list *uris, enum uri_type type, bool use_rrdp)
+static int
+download_uris(struct uri_list *uris, enum uri_type type, bool use_rrdp,
+    uris_dl_cb cb, void *arg)
 {
-	struct rpki_uri **cursor, *uri;
-	ARRAYLIST_FOREACH(uris, cursor) {
-		uri = *cursor;
-		if (uri_get_type(uri) == type && download(uri, use_rrdp) == 0)
-			return uri;
+	struct rpki_uri **uri;
+	int error;
+
+	ARRAYLIST_FOREACH(uris, uri) {
+		if (uri_get_type(*uri) == type) {
+			error = download(*uri, use_rrdp, cb, arg);
+			if (error <= 0)
+				return error;
+		}
 	}
-	return NULL;
+
+	return 1;
 }
 
 /**
@@ -622,41 +634,42 @@ download_uris(struct uri_list *uris, enum uri_type type, bool use_rrdp)
  *
  * Sequentially (in the order dictated by their priorities) attempts to update
  * (in the cache) the content pointed by each URL.
- * Stops on the first success, returning the corresponding URI.
+ * If a download succeeds, calls cb on it. If cb succeeds, returns without
+ * trying more URLs.
  *
- * If there's no successful update, attempts to find one that's already cached.
- * Returns the newest successfully cached URI.
- *
- * Does not grab any references.
+ * If none of the URLs download and callback properly, attempts to find one
+ * that's already cached, and callbacks it.
  */
-struct rpki_uri *
-uris_download(struct uri_list *uris, bool use_rrdp)
+int
+uris_download(struct uri_list *uris, bool use_rrdp, uris_dl_cb cb, void *arg)
 {
 	struct rpki_uri **cursor, *uri;
+	int error;
 
 	if (config_get_http_priority() > config_get_rsync_priority()) {
-		uri = download_uris(uris, UT_HTTPS, use_rrdp);
-		if (uri != NULL)
-			return uri;
-		uri = download_uris(uris, UT_RSYNC, use_rrdp);
-		if (uri != NULL)
-			return uri;
+		error = download_uris(uris, UT_HTTPS, use_rrdp, cb, arg);
+		if (error <= 0)
+			return error;
+		error = download_uris(uris, UT_RSYNC, use_rrdp, cb, arg);
+		if (error <= 0)
+			return error;
 
 	} else if (config_get_http_priority() < config_get_rsync_priority()) {
-		uri = download_uris(uris, UT_RSYNC, use_rrdp);
-		if (uri != NULL)
-			return uri;
-		uri = download_uris(uris, UT_HTTPS, use_rrdp);
-		if (uri != NULL)
-			return uri;
+		error = download_uris(uris, UT_RSYNC, use_rrdp, cb, arg);
+		if (error <= 0)
+			return error;
+		error = download_uris(uris, UT_HTTPS, use_rrdp, cb, arg);
+		if (error <= 0)
+			return error;
 
 	} else {
 		ARRAYLIST_FOREACH(uris, cursor) {
-			uri = *cursor;
-			if (download(uri, use_rrdp) == 0)
-				return uri;
+			error = download(*cursor, use_rrdp, cb, arg);
+			if (error <= 0)
+				return error;
 		}
 	}
 
-	return cache_recover(uris, use_rrdp);
+	uri = cache_recover(uris, use_rrdp);
+	return (uri != NULL) ? cb(uri, arg) : ESRCH;
 }

src/types/uri.h
@@ -56,6 +56,15 @@ void uris_init(struct uri_list *);
 void uris_cleanup(struct uri_list *);
 
 void uris_add(struct uri_list *, struct rpki_uri *);
-struct rpki_uri *uris_download(struct uri_list *, bool);
+
+/*
+ * The callback should return
+ *
+ * - 0 on success ("URI handled successfully")
+ * - > 0 on soft errors ("Try another URI")
+ * - < 0 on hard errors ("Abandon foreach")
+ */
+typedef int (*uris_dl_cb)(struct rpki_uri *, void *);
+int uris_download(struct uri_list *, bool, uris_dl_cb, void *);
 
 #endif /* SRC_TYPES_URI_H_ */